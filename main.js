99409df6a5e44b7a15c737d53bdc6f042e4abc4d
mils2001 <Mileschris466@gmail.com>
2023-09-16 14:13:00 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,93 @@
+const x=20
+const colors=x>9? 'sugar':'salt';
+
+switch (colors){
+    case'sugar':
+    console.log('color is sugar');
+    break;
+    case'salt':
+    console.log('color is salt');
+    break;
+    default:
+    console.log('color is not sugar or salt');
+    break;
+}
+function addnums(num1,num2){
+    return num1+num2
+}
+addnums(5,4);
+var answer = addnums(5,4);
+console.log(answer);
+
+var nums = [3,6,2,5,9]
+
+var fruits = ["mango", "pineapple", "apple", "plums", "orange"]
+
+// write a function that takes an array and prints its contents
+
+// write a function that takes an array and a parameter to searh in that
+// array. If the item is found the function returns true otherwise return
+// false
+
+console.log(nums[1])
+
+console.log(fruits[0])
+for(let i=0;i<fruits.length;i++){
+    console.log(fruits[i]);
+}
+function add(a,b){
+    console.log(a+b);
+}
+add(20,30);
+
+function subtract(a,b){
+    console.log(a-b);
+}
+subtract (100,75);
+
+var add2= function(a,b){
+    let sum=  a+b
+    console.log(sum)
+}
+add2(437,233);
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
